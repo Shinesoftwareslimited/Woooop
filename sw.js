285e41a10d00ded040187b3b909d678e5d2cd39c
Shinesoftwareslimited <innovativetechoabhi@gmail.com>
2024-09-08 10:41:07 +0530

    sw.js

---
+++ sw.js
@@ -0,0 +1,49 @@
+const CACHE_NAME = "newseepie-cache-v1";
+const urlsToCache = [
+  "newseepie.live",
+  "index.html",
+  "rset.css",
+  "/script.js",
+  "OIG2.png",
+  "OIG2.png"
+];
+
+// Install the service worker
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log("Opened cache");
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Cache and return requests
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
+
+// Update the service worker
+self.addEventListener("activate", event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
