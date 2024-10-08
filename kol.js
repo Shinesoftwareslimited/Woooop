1dd6eb056bba520b3b90fbbc0305a224f7348c14
Shinesoftwareslimited <innovativetechoabhi@gmail.com>
2024-10-08 07:04:22 +0530

    kol.js

---
+++ kol.js
@@ -0,0 +1,6 @@
+// Optional JavaScript if you want to track clicks or add dynamic functionality
+document.querySelectorAll('.download-btn').forEach(button => {
+    button.addEventListener('click', () => {
+        console.log('File download initiated.');
+    });
+});
